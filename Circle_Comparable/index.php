c766a5c16f36b3347f0f20027ff490f77677a318
cuong6218 <cuong6218@domain.com>
2020-06-29 10:37:29 +0700

    update file with comparator

--- Circle_Comparable/index.php
+++ Circle_Comparable/index.php
@@ -1,7 +1,13 @@
 <?php
-include_once('ComparableCircle.php');
+// include_once('ComparableCircle.php');
+include_once('CircleComparator.php');
 
-$circleOne = new ComparableCircle('circleOne', 8);
-$circleTwo = new ComparableCircle('circleTwo', 2);
+// $circleOne = new ComparableCircle('circleOne', 8);
+// $circleTwo = new ComparableCircle('circleTwo', 2);
 
-var_dump($circleOne->compareTo($circleTwo));
+// var_dump($circleOne->compareTo($circleTwo));
+
+$circleOne = new Circle('circleOne' , 6);
+$circleTwo = new Circle('circleTwo' , 4);
+$circleComparator = new CircleComparator();
+var_dump($circleComparator->compare($circleOne, $circleTwo));
